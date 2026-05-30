8e1a83011df403ac4763664f8d58844e7dc21e5f
Mohammad Ashraf <mohammadashraf09082004@gmail.com>
2026-05-30 20:30:56 +0300

    online web site

--- Main/db_connection.php
+++ Main/db_connection.php
@@ -1,13 +1,18 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "skill_step";
+$host = "mysql-1e6a3a9d-skill-step.l.aivencloud.com";
+$user = "avnadmin";
+$pass = "AVNS_p5NpGhFdOk8dRo1Ek6f"; // ضع الباسورد الحقيقي الخاص بك هنا
+$db   = "defaultdb"; // هذا اسم الداتابيز الافتراضية في Aiven التي رفعنا عليها الجداول
+$port = 21164;
 
-$conn = mysqli_connect($host, $username, $password, $dbname);
+// إنشاء الاتصال الأونلاين
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
+// فحص الاتصال
 if (!$conn) {
-    die("Failed to connect to database: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
-mysqli_set_charset($conn, "utf8");
+
+// دعم اللغة العربية
+mysqli_set_charset($conn, "utf8mb4");
 ?>
