54474a6604bf4ec8a90cdb5dcec0512d59eb66a8
Mohammad Ashraf <mohammadashraf09082004@gmail.com>
2026-05-30 23:57:27 +0300

    Replace PHP redirect with HTML redirect

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<?php
-// تحويل تلقائي للمسار الشغال 100%
-header("Location: login/Login.php");
-exit;
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Skill-Step</title>
+    <script>
+        // تحويل إجباري ومضمون عن طريق المتصفح مباشرة للرابط الشغال
+        window.location.href = "login/Login.php";
+    </script>
+</head>
+<body>
+    <p>جاري الانتقال إلى صفحة تسجيل الدخول...</p>
+</body>
+</html>
